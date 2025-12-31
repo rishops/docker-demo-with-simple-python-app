c672e51d43b72f127ec5870f839c35117069035a
Rishabh <rishabh.bohra@opstree.com>
2025-12-31 13:19:28 +0530

    Fix Bandit security issues: B602 (subprocess) and B104 (hardcoded bind)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import subprocess
 import shlex
+import os
 
 from flask import Flask, request, jsonify
 
@@ -29,4 +30,6 @@ def server_error(e):
     """.format(e), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', '8080'))
+    app.run(host=host, port=port)
